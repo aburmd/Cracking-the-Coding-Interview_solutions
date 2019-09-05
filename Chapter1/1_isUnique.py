36012b526a5907a432a755d5ea25bf2e84b7e0b9
abuhura <abuhura@amazon.com>
2019-09-04 23:01:00 -0700

    added questions to each solution

--- Chapter1/1_isUnique.py
+++ Chapter1/1_isUnique.py
@@ -1,3 +1,7 @@
+#Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+#Hints: #44, #7 7 7, #732
+
+
 def isUnique(string):
 	a=[];
 	for i in string:
--- Chapter1/2_isPermutation.py
+++ Chapter1/2_isPermutation.py
@@ -1,3 +1,6 @@
+#Check Permutation: Given two strings,write a method to decide if one is a permutation of the other.
+#Hints: #7, #84, #722, #737
+
 import heapq
 
 def isPermutation(string1,string2):
